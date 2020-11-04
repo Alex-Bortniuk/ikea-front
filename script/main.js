aca76d48f2a9ca5317ff33573cd6aea22c3a7043
Alex Bortniuk <sanya.bortniuk@gmail.com>
2020-11-04 11:13:38 +0200

    работаем над DOM елементами

---
+++ script/main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+(function() {
+
+    const btnBurger = document.querySelector(".btn-burger");
+    const catalog = document.querySelector(".catalog");
+    const overlay = document.querySelector(".overlay");
+    const btnClose = document.querySelector(".btn-close");
+    // const catalogList = document.querySelector(".catalog-list");
+    const subCatalog = document.querySelector(".subcatalog");
+    const subCatalogHeader = document.querySelector(".subcatalog-header");
+
+
+
+    function openMenu(e) {
+        catalog.classList.add("open");
+        overlay.classList.add("active");
+    }
+
+    function closeMenu(e) {
+        catalog.classList.remove("open");
+        overlay.classList.remove("active");
+        subCatalog.classList.remove("subopen");
+    }
+
+
+    function openSubMenu(e) {
+        e.preventDefault();
+        const itemList = e.target.closest(".catalog-list__item");
+        if(itemList) {
+            subCatalogHeader.innerHTML = itemList.innerHTML;
+            subCatalog.classList.add("subopen");
+        }
+    }
+
+    btnBurger.addEventListener("click", openMenu);
+    btnClose.addEventListener("click", closeMenu);
+    catalog.addEventListener("click", openSubMenu);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
